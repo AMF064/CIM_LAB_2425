2101db82b93c250dbd36e41b1dad22f90cd100d0
AMF064 <114866865+AMF064@users.noreply.github.com>
2025-02-12 22:42:54 +0100

    First commit

---
+++ GenSen.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity GenSen is
+    port (Clk, Reset : in std_logic;
+          per : in std_logic(1 downto 0);
+          led : out signed(7 downto 0);
+          dac : out unsigned(7 downto 0));
+end GenSen;
+
+-- Grupo 20:
+--     00 :  600
+--     01 : 1000
+--     10 : 2200
+--     11 : 3900
+
+architecture behaviour of GenSen is
+begin
+end architecture;
