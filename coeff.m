5e551ecd200b026941fca65e497e2d8a855f451a
AMF064 <114866865+AMF064@users.noreply.github.com>
2025-02-24 20:35:01 +0100

    Good coefficients, finally

--- coeff.m
+++ coeff.m
@@ -1,5 +1,6 @@
 nums = [0.0007 -0.0070 0.0368 -0.1376 0.6071 0.6071 -0.1376 0.0368 -0.0070 0.0007];
-constant = 127 / max(nums);
-coeffs = constant * nums';
-disp(round(coeffs));
-disp(["Constant: " num2str(constant)]);
+k = floor(log2(127/max(nums)))
+coeffs = 2^k * nums';
+assert(round_coeffs < 2^8);
+round_coeffs = round(coeffs)
+constant = 2^k
