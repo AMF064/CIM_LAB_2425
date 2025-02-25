f0c4b458b8079fea82d2a74edfd24f028f7c4d7a
AMF064 <114866865+AMF064@users.noreply.github.com>
2025-02-25 11:43:05 +0100

    Make the code at least work

--- FIR.vhd
+++ FIR.vhd
@@ -3,16 +3,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity FIR is
-    generic (a0 : integer;
-             a1 : integer;
-             a2 : integer;
-             a3 : integer;
-             a4 : integer;
-             a5 : integer;
-             a6 : integer;
-             a7 : integer;
-             a8 : integer;
-             a9 : integer);
+    generic (a0 : integer range -128 to 127;
+             a1 : integer range -128 to 127;
+             a2 : integer range -128 to 127;
+             a3 : integer range -128 to 127;
+             a4 : integer range -128 to 127;
+             a5 : integer range -128 to 127;
+             a6 : integer range -128 to 127;
+             a7 : integer range -128 to 127;
+             a8 : integer range -128 to 127;
+             a9 : integer range -128 to 127);
     port (Clk, Reset, Enable : in std_logic;
           DataIn : in signed(7 downto 0);
           DataOut : out signed(7 downto 0));
@@ -23,27 +23,55 @@ end FIR;
 architecture parallel of FIR is
     type buffer_t is array(integer range 0 to 9) of signed(7 downto 0);
     signal sample_buffer : buffer_t;
+    constant constants : buffer_t := (
+    to_signed(a0, 8),
+    to_signed(a1, 8),
+    to_signed(a2, 8),
+    to_signed(a3, 8),
+    to_signed(a4, 8),
+    to_signed(a5, 8),
+    to_signed(a6, 8),
+    to_signed(a7, 8),
+    to_signed(a8, 8),
+    to_signed(a9, 8));
+    type aux_mult_t is array(integer range 0 to 9) of signed(15 downto 0);
+    signal aux_mult : aux_mult_t;
+    signal out_s : signed(25 downto 0);
 begin
-    process (Clk, Reset)
+    calculation: process (Clk, Reset)
     begin
         if Reset = '0' then
-            DataOut <= (others => '0');
+            out_s <= (others => '0');
             sample_buffer <= (others => (others => '0'));
         elsif rising_edge(Clk) then
             if Enable = '1' then
                 sample_buffer <= DataIn & sample_buffer(0 to 8);
-                DataOut <= a0 * sample_buffer(0) +
-                           a1 * sample_buffer(1) +
-                           a2 * sample_buffer(2) +
-                           a3 * sample_buffer(3) +
-                           a4 * sample_buffer(4) +
-                           a5 * sample_buffer(5) +
-                           a6 * sample_buffer(6) +
-                           a7 * sample_buffer(7) +
-                           a8 * sample_buffer(8) +
-                           a9 * sample_buffer(9);
+                for i in 0 to 9 loop
+                    aux_mult(i) <= constants(i) * sample_buffer(i);
+                end loop;
+                out_s <= resize(aux_mult(0), out_s'length) +
+                         resize(aux_mult(1), out_s'length) +
+                         resize(aux_mult(2), out_s'length) +
+                         resize(aux_mult(3), out_s'length) +
+                         resize(aux_mult(4), out_s'length) +
+                         resize(aux_mult(5), out_s'length) +
+                         resize(aux_mult(6), out_s'length) +
+                         resize(aux_mult(7), out_s'length) +
+                         resize(aux_mult(8), out_s'length) +
+                         resize(aux_mult(9), out_s'length);
             else
-                DataOut <= (others => '0');
+                out_s <= (others => '0');
+            end if;
+        end if;
+    end process;
+
+    register_out: process(Clk, Reset)
+    begin
+        if Reset = '0' then
+            DataOut <= (others => '0');
+        elsif rising_edge(Clk) then
+            if Enable = '1' then
+                DataOut <= resize(out_s, 8);
             end if;
         end if;
     end process;
