36cad76b7d9710b4baa781137ee695a3a2277d12
AMF064 <114866865+AMF064@users.noreply.github.com>
2025-02-26 10:32:03 +0100

    Reorder some things, start over with the pipeline

--- FIR.vhd
+++ FIR.vhd
@@ -25,6 +25,9 @@ architecture parallel of FIR is
     signal sample_buffer : buffer_t;
     signal out_s : signed(25 downto 0);
 begin
+
+    DataOut <= out_s(16 downto 9);
+
     calculation: process (Clk, Reset)
     begin
         if Reset = '0' then
@@ -46,37 +49,25 @@ begin
             end if;
         end if;
     end process;
-
-    DataOut <= out_s(16 downto 9);
 end architecture;
 
--- architecture pipelined of FIR is
---     type buffer_t is array(integer range 0 to 9) of signed(7 downto 0);
---     type pipe_buffer_t is array(integer range 0 to 8) of signed(7 downto 0);
---     signal intermed_buffer : pipe_buffer_t;
---     signal sample_buffer : buffer_t;
--- begin
---     process (Clk, Reset)
---     begin
---         if Reset = '0' then
---             DataOut <= (others => '0');
---             intermed_buffer <= (others => (others => '0'));
---         elsif rising_edge(Clk) then
---             if Enable = '1' then
---                 sample_buffer <= DataIn & sample_buffer(1 to 8);
---                 intermed_buffer(0) <= a0 * sample_buffer(0);
---                 intermed_buffer(1) <= intermed_buffer(0) + a1 * sample_buffer(1);
---                 intermed_buffer(2) <= intermed_buffer(1) + a2 * sample_buffer(2);
---                 intermed_buffer(3) <= intermed_buffer(2) + a3 * sample_buffer(3);
---                 intermed_buffer(4) <= intermed_buffer(3) + a4 * sample_buffer(4);
---                 intermed_buffer(5) <= intermed_buffer(4) + a5 * sample_buffer(5);
---                 intermed_buffer(6) <= intermed_buffer(5) + a6 * sample_buffer(6);
---                 intermed_buffer(7) <= intermed_buffer(6) + a7 * sample_buffer(7);
---                 intermed_buffer(8) <= intermed_buffer(7) + a8 * sample_buffer(8);
---                 DataOut <= intermed_buffer(8) + a9 * sample_buffer(9);
---             else
---                 DataOut <= (others => '0');
---             end if;
---         end if;
---     end process;
--- end architecture;
+architecture pipelined of FIR is
+    type buffer_t is array(integer range 0 to 9) of signed(7 downto 0);
+    signal sample_buffer : buffer_t;
+    signal out_s : signed(25 downto 0);
+begin
+
+    DataOut <= out_s(16 downto 9);
+
+    process (Clk, Reset)
+    begin
+        if Reset = '0' then
+            out_s <= (others => '0');
+            sample_buffer <= (others => '0');
+        elsif rising_edge(Clk) then
+            if Enable = '1' then
+                sample_buffer <= DataIn & sample_buffer(0 to 7);
+            end if;
+        end if;
+    end process;
+end architecture;
