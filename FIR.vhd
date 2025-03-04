96df06036cf037b602a588184eb2b8174099f429
AMF064 <114866865+AMF064@users.noreply.github.com>
2025-03-04 08:13:46 +0100

    Use the parallel architecture by default.
    Uncomment the pipelined one to use it (commenting out the other one).

--- FIR.vhd
+++ FIR.vhd
@@ -51,37 +51,37 @@ begin
     end process;
 end architecture;
 
-architecture pipelined of FIR is
-    type buffer_t is array(integer range 0 to 10) of signed(7 downto 0);
-    type inter_buf_t is array(integer range 0 to 2) of signed(24 downto 0);
-    signal sample_buffer : buffer_t;
-    signal intermed_buffer : inter_buf_t;
-    signal out_s : signed(25 downto 0);
-begin
-
-    DataOut <= out_s(16 downto 9);
-
-    process (Clk, Reset)
-    begin
-        if Reset = '0' then
-            out_s <= (others => '0');
-            sample_buffer <= (others => (others => '0'));
-            intermed_buffer <= (others => (others => '0'));
-        elsif rising_edge(Clk) then
-            if Enable = '1' then
-                sample_buffer <= DataIn & sample_buffer(0 to (sample_buffer'right - 1));
-                intermed_buffer(0) <= resize(a0 * DataIn, intermed_buffer(0)'length);
-                -- Se incrementa el indice que se lee una vez mas por cada registro que se pone
-                intermed_buffer(1) <= intermed_buffer(0) + resize(a1 * sample_buffer(0 + 1), intermed_buffer(0)'length);
-                intermed_buffer(2) <= intermed_buffer(1) + resize(a2 * sample_buffer(1 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a3 * sample_buffer(2 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a4 * sample_buffer(3 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a5 * sample_buffer(4 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a6 * sample_buffer(5 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a7 * sample_buffer(6 + 1 + 1), intermed_buffer(0)'length) +
-                                      resize(a8 * sample_buffer(7 + 1 + 1), intermed_buffer(0)'length);
-                out_s <= intermed_buffer(2) + resize(a9 * sample_buffer(10), out_s'length);
-            end if;
-        end if;
-    end process;
-end architecture;
+-- architecture pipelined of FIR is
+--     type buffer_t is array(integer range 0 to 10) of signed(7 downto 0);
+--     type inter_buf_t is array(integer range 0 to 2) of signed(24 downto 0);
+--     signal sample_buffer : buffer_t;
+--     signal intermed_buffer : inter_buf_t;
+--     signal out_s : signed(25 downto 0);
+-- begin
+--
+--     DataOut <= out_s(16 downto 9);
+--
+--     process (Clk, Reset)
+--     begin
+--         if Reset = '0' then
+--             out_s <= (others => '0');
+--             sample_buffer <= (others => (others => '0'));
+--             intermed_buffer <= (others => (others => '0'));
+--         elsif rising_edge(Clk) then
+--             if Enable = '1' then
+--                 sample_buffer <= DataIn & sample_buffer(0 to (sample_buffer'right - 1));
+--                 intermed_buffer(0) <= resize(a0 * DataIn, intermed_buffer(0)'length);
+--                 -- Se incrementa el indice que se lee una vez mas por cada registro que se pone
+--                 intermed_buffer(1) <= intermed_buffer(0) + resize(a1 * sample_buffer(0 + 1), intermed_buffer(0)'length);
+--                 intermed_buffer(2) <= intermed_buffer(1) + resize(a2 * sample_buffer(1 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a3 * sample_buffer(2 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a4 * sample_buffer(3 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a5 * sample_buffer(4 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a6 * sample_buffer(5 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a7 * sample_buffer(6 + 1 + 1), intermed_buffer(0)'length) +
+--                                       resize(a8 * sample_buffer(7 + 1 + 1), intermed_buffer(0)'length);
+--                 out_s <= intermed_buffer(2) + resize(a9 * sample_buffer(10), out_s'length);
+--             end if;
+--         end if;
+--     end process;
+-- end architecture;
